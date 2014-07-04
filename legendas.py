fcdc3507e2e8a5a553d8d8ea0e749566b437ff6d
jonacola <jt.carpinteiro@gmail.com>
2014-07-04 01:14:11 +0100

    Continue to manage Files And Directory

--- legendas.py
+++ legendas.py
@@ -1,6 +1,5 @@
 import os.path
 import hashlib
-import wget
 import sys
 import urllib2
 import requests
@@ -9,6 +8,7 @@ import urllib2
 import glob
 from os import listdir
 from os.path import isfile, join
+import collections
 
 #install wget , requests
 base_url = 'http://api.thesubdb.com/?{0}'
@@ -18,6 +18,8 @@ my_path = "/home/carpinteiro/WorkSpace/Python/"
 config = 'config.txt'
 diretorias = []
 ficheiros = []
+nl = []
+nnl = []
 
 #verificar se o ficheiro existe
 '''if not os.path.isfile(HASHES_FILE):
@@ -63,16 +65,44 @@ def check_language(filename):
 	response = urllib2.urlopen(req)
 	print response.info()
 	file = open(filename,"wb")
+	file.write("Linguagens disponiveis para download:\n")
 	file.write(response.read())
 
-
+##METODO ESTA NO GET_ALL FILES
 def have_subtitle(filename,diretoriaSearch):
 	diretoria = os.listdir(diretoriaSearch)
-	ext = filename.split(".")
-	contem = ext.pop(len(ext)-1)
-	print contem
-	#print(ext.index(len(ext)-1))
 	
+	#lista com as extensoes dos ficheiros
+	ext = filename.split(".")
+	sem_ext = ext.pop(len(ext)-1)
+	#print sem_ext
+
+	nf = filename[:-4]
+	print nf
+	if(sem_ext == 'mkv' or sem_ext == 'srt' or sem_ext == 'mp4'):
+	#nome dos ficheiros sem a extensao
+		nl.append(nf)
+	print("lista")
+	print nl
+	nova = [x for x, y in collections.Counter(nl).items() if y > 1]
+		
+	
+	
+
+	#sem_ext = ext.pop(len(ext)-1)
+	#print sem_ext
+	#print(ext.index(len(ext)-1))
+def list_duplicates(seq):
+  seen = set()
+  seen_add = seen.add
+  # adds all elements it doesn't know yet to seen and all other to seen_twice
+  seen_twice = set( x for x in seq if x in seen or seen_add(x) )
+  # turn the set into a list (as requested)
+  return list( seen_twice )
+
+def remove_duplicates(l):
+    return list(set(l))
+
 def get_all_files(diretoriaSearch):
 	diretoria = os.listdir(diretoriaSearch)
         for file in diretoria:
@@ -82,9 +112,32 @@ def get_all_files(diretoriaSearch):
         	else:
         		ficheiros.append(file)
         		#print file
+        print"\nFicheiros:"
         for f in ficheiros:
-        	have_subtitle(f,diretoriaSearch)
-        	print f
+        	#have_subtitle(f,diretoriaSearch)
+        	#print f
+        	ext = f.split(".")
+        	sem_ext = ext.pop(len(ext)-1)
+        	nf = f[:-4]
+        	#print nf
+        	if(sem_ext == 'mkv' or sem_ext == 'mp4'):
+        		nl.append(nf)
+        	elif(sem_ext == 'srt'):
+        		nnl.append(nf)
+       	#list(set(l) - set(l2))
+    	print"\nDiretorias:"
+    	for p in diretorias:
+    		print p
+    		#get_all_files(p)
+    	#nova = [x for x, y in collections.Counter(nl).items() if x > 0]
+    	#print nl
+    	#print nnl
+    	#seen = set()
+    	#seen_add = seen.add
+    	#nova = [ x for x in nl if not (x in seen() or seen_add(x))]
+    	#nova = remove_duplicates(nl)
+    	nova = list(set(nl) - set(nnl))
+    	print nova
 
 
 def main(argv):
@@ -93,11 +146,13 @@ def main(argv):
         sys.exit (1)
     else:
         #download_subtitle(get_hash('Californication.S07E05.720p.HDTV.x264-2HD.mkv'),'pt,en','Californication.S07E05.720p.HDTV.x264-2HD.srt')
+        #download_subtitle(get_hash('Petals.On.The.Wind.2014.HDRip.XviD-EVO.avi'),'pt,en','Petals.On.The.Wind.2014.HDRip.XviD-EVO.srt')
+        #download_subtitle(get_hash('Game of Thrones S02E01.mp4'),'pt,en','Game of Thrones S02E01.srt')
         #print glob.glob("/home/carpinteiro/WorkSpace/Python/*")
-        onlyfiles = [ f for f in listdir(my_path) if isfile(join(my_path,f)) ]
-        print onlyfiles
+        #onlyfiles = [ f for f in listdir(my_path) if isfile(join(my_path,f)) ]
+        #print onlyfiles
 
-        check_language(config)
+        #check_language(config)
         get_all_files(my_path)
 
 
